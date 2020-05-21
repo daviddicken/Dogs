d293984b1ee3b01c2903858a88f17e453948288d
daviddicken <davidfromseattle@gmail.com>
2020-05-21 12:58:53 -0700

    converted annoy box into a function that takes a parameter

--- annoyBox.js
+++ annoyBox.js
@@ -1,14 +1,25 @@
 'use strict'
 
-var yesNo = prompt ("This boxes sole purpose is to annoy you!  Did it work?" );
+// get variable from user to pass into function
+var yesNo = prompt ("This boxes sole purpose is to annoy you!  Did it work?" ).toLowerCase();
 
-if (yesNo == "yes"){
+function moreBoxes(answer){
+    
+if (answer == "yes"){
     alert("Thank you, I feel complete now.")
 }
-else if (yesNo == "no"){
+else if (answer == "no"){
     confirm("How about this one? Just hit OK if I annoyed you a little bit.")
 }
 else{
-    alert("I don't understand your jibberish. Your either not making any since or your letters are to big. I only understand lower case letters")
+    alert("I don't understand your jibberish. It must be you because now I understand uppercase case letters")
 }
+
+}
+
+// call on function and pass in variable
+moreBoxes(yesNo);
+
+
+
 
