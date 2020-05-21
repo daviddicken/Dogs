f6893cbd516b04bad3a87d300a877296ca64dd5e
daviddicken <davidfromseattle@gmail.com>
2020-05-20 23:18:28 -0700

    added annoy box

---
+++ annoyBox.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var yesNo = prompt ("This boxes sole purpose is to annoy you!  Did it work?" );
+
+if (yesNo == "yes"){
+    alert("Thank you, I feel complete now.")
+}
+else if (yesNo == "no"){
+    confirm("How about this one? Just hit OK if I annoyed you a little bit.")
+}
+else{
+    alert("I don't understand your jibberish. Your either not making since or your letters are to big. I only understand lower case letters")
+}
+
